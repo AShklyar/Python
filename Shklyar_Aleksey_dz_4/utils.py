616c4815293bea19098db900b6db5def931565de
AShklyar <a.shklyar84@gmail.com>
2021-06-04 15:40:25 +0500

    модули В первом задании пришлось повозиться и разобратьс с прокси так как pip после того как изменил виртуальное окружение перестал понимать как ему получить доступ к репозиторию. Во втором задании пришлось разобраться почему Decimal не хотел конвертировать str в decimal. Оказалось, что разделитьель десятичной части имеет значание. В третьем получить дату из строки не составило труда, но вот привести ее к типу date... пришлоп покапать в интернете. Четвертое задание не вызвало сложностей. В пятом пришлось опять повозиться с прокси.. так как из PyCharm отрабатывало корректно, а в терминале опять не получалось. В итоге решил сделать отдельный модуль utils_proxy.py для запуска с параметрами прокси. Можно было конечно сделать более уневирсальный вариант. Да и видимо ошибку надо как-то обрбатывать, когда request не может получить данные из интернета. В итоге 5 задание получилось, прикладываю скрин.

---
+++ Shklyar_Aleksey_dz_4/utils.py
@@ -0,0 +1,25 @@
+from requests import get
+from decimal import Decimal
+import datetime
+
+
+def currency_rates(some_cash):
+    """ Function takes some currency and returns current values from site cbr.ru
+        some_cash str type
+        """
+    get_val = get('http://www.cbr.ru/scripts/XML_daily.asp')
+    date_conversation = get_val.text[get_val.text.find('Date')+6:get_val.text.find('Date')+16]
+    date_conversation = datetime.datetime.strptime(date_conversation,"%d.%m.%Y")
+    # print(type(date_conversation)) так можно проверить что действительно объект типа date
+    # переводим в верхний регистра
+    some_cash = some_cash.upper()
+    # знаяю структуру XML, зная что валюта обозначается 3 заглавными буквами можно сразу
+    # посмотрет есть такая валюта в строке или нет
+    if some_cash in get_val.text:
+        start = get_val.text.find('<Value>',get_val.text.find(some_cash))  # смотрим первое вхождение <Value> после того
+                                                                           # как нашли валюту
+        value = get_val.text[start+7:start+14]
+        value = value.replace(',','.')  # для Decimal оказалось критично, меняем запятую на точку
+        print(f'{some_cash} {Decimal(value).quantize(Decimal("1.00"))} {date_conversation.strftime("%d.%m.%Y")}') # выводим значание и округляем его до сотых
+    else:
+        print('Такой валюты нет')
